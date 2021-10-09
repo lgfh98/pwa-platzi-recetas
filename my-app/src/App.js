dbae0ab93d84e01ee2f6f91a24c3bba071036c1f
lgfh98 <lgfigueroah@outlook.com>
2021-10-09 10:56:10 -0500

    google analytics + offline

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Router, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
 import Timer from "./pages/Timer";
 import "./App.css";
 
+import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
+
+const history = createBrowserHistory();
+
+ReactGA.initialize("UA-000000-01");
+
+// Para analizar la pagina inicial
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+// Para analizar las otras paginas
+history.listen(function (location) {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+});
+
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <header>
             <Link to="/">Recetas</Link>
@@ -20,7 +35,7 @@ export default class App extends React.Component {
             <Route path="/timer" component={Timer} />
           </main>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
